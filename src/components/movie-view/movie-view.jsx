6ffcfd9495882fb27f2137b4f250bda2e9126f4c
oliviatruscott <103083352+oliviatruscott@users.noreply.github.com>
2023-02-25 19:08:36 -0800

    rewote movieview return statement using bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,23 +1,34 @@
-import { useParams } from "react-router";
-import { Link } from "react-router-dom";
+import { useParams } from 'react-router';
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 export const MovieView = ({ movies }) => {
     const { movieId } = useParams();
     const movie = movie.find((b) => movie.id === movieId);
-    
+
     return (
-        <div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.director}</span>
-            </div>
-            <Link to={`/`}>
-                <button className="back-button">Back</button>
-            </Link>
-        </div>
+        <>
+            <Row className='d-flex flex-row-reverse p-3'>
+                <Col md={7} className='d-flex flex-column'>
+                    <Row className='d-flex flex-row  justify-content-between'>
+                        <Col md={9} className='d-flex flex-column'>
+                            <h3 className='my-0'>
+                                <span>Title: </span>
+                                <span>{movie.title}</span>
+                            </h3>
+                            <h5 className='mt-1 text-left text-muted'>
+                                <span>Director: </span>
+                                <span>{movie.director.name}</span>
+                            </h5>
+                        </Col>
+
+                        <Col md={3} className='align-self-end mb-2 text-end'>
+                            <span>Genre: </span>
+                            <span className='fw-bolder'>{movie.genre.name}</span>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </>
     );
 };
