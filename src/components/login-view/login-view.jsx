d6783af8ba3aa54d9b22d9114e30a79325ad668f
oliviatruscott <103083352+oliviatruscott@users.noreply.github.com>
2023-02-25 19:06:52 -0800

    update loginview

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { Button, Form, Container } from "react-bootstrap";
 
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    
     const handleSubmit = (event) => {
         //prevents default reload of entire page
         event.preventDefault();
         const data = {
-            access: username,
-            secret: password
+            username: username,
+            password: password
         };
         fetch("https://pacific-taiga-63279.herokuapp.com/login", {
             methon: "POST",
@@ -19,45 +19,48 @@ export const LoginView = ({ onLoggedIn }) => {
             },
             body: JSON.stringify(data)
         })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("login response: ", data);
-            if (data.user) {
-                localStorage.setItem("user", JSON.stringify(data.user));
-                localStorage.setItem("token", data.token);
-                onLoggedIn(data.user, data.token);
-            } else {
-                alert("No such user");
-            }
-        })
-        .catch((e) => {
-            alert("Something went wrong");
-        });
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("login response: ", data);
+                if (data.user) {
+                    localStorage.setItem("user", JSON.stringify(data.user));
+                    localStorage.setItem("token", data.token);
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert("No such user");
+                }
+            })
+            .catch((e) => {
+                alert("Something went wrong");
+            });
     };
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
-                <Form.Control
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                    minLength="3"
-                />
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-                <Form.Label>Password:</Form.Label>
-                <Form.Control
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </Form.Group>
-            <Button varent="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+        <Container>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formUsername">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                        minLength="3"
+                    />
+                </Form.Group>
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </Form.Group>
+                <br></br>
+                <Button varent="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
     );
 };
