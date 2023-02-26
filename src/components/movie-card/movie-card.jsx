3deb321e242a80cda43470c745be58008f5e888d
oliviatruscott <103083352+oliviatruscott@users.noreply.github.com>
2023-02-25 19:12:28 -0800

    implemented favorite icon to movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -14,6 +14,11 @@ export const MovieCard = ({ movie }) => {
                         Open
                     </Button>
                 </Link>
+                <FavoriteIcon
+                    user={user}
+                    movie={movieData}
+                    updateUserOnFav={updateUserOnFav}
+                />
             </Card.Body>
         </Card>
     );
